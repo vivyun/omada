5c543c95a2a4d75a7c7bd546ca650088a361f366
vivyun <viviannguyen951@gmail.com>
2025-07-15 11:10:36 -0700

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,32 @@
 <main class="main-container">
   <section class="section-container">
-    <div class="flex flex-col gap-8 max-w-[1920px] mx-auto py-16 lg:py-28 px-8 lg:px-16">
+    <div class="flex flex-col gap-12 max-w-[1920px] mx-auto py-16 lg:py-28 px-8 lg:px-16">
       <h1>Omada Pages, Components, and Design System</h1>
-      <h3 class="mt-12">SvelteKit + Tailwind</h3>
-      <a class="link" href="demo-kit">Demo Kit</a>
-      <h3 class="mt-12">Vanilla</h3>
-      <a class="link" href="demo-kit-vanilla/index.html">Demo Kit Vanilla</a>
-      <h3 class="mt-12">Existing Pages</h3>
-      <a class="link" href="long-range/index.html">Long Range (source code)</a>
-      <a class="link" href="long-range/old.html">Long Range (old source code)</a>
-      <a class="link" href="outdoor/index.html">Outdoor (source code)</a>
-      <a class="link" href="case-studies/index.html">Case Studies (from page source)</a>
-      <a class="link" href="contact/index.html">Contact (source code)</a>
-      <a class="link" href="contact/live.html">Contact (from page source)</a>
+      <div class="grid grid-cols-1 lg:grid-cols-5 gap-8">
+        <div class="flex flex-col gap-4">
+          <h3>Demo Kit</h3>
+          <a class="link" href="demo-kit">SvelteKit Page</a>
+          <a class="link" href="demo-kit-vanilla/index.html">Live Vanilla Page</a>
+        </div>
+        <div class="flex flex-col gap-4">
+          <h3>PtP & PtMP Wireless Bridges</h3>
+          <a class="link" href="long-range/index.html">Source code</a>
+          <a class="link" href="long-range/old.html">Old source code (template)</a>
+        </div>
+        <div class="flex flex-col gap-4">
+          <h3>Outdoor Wi-Fi</h3>
+          <a class="link" href="outdoor/index.html">Source code</a>
+        </div>
+        <div class="flex flex-col gap-4">
+          <h3>Case Studies</h3>
+          <a class="link" href="case-studies/index.html">Page source</a>
+        </div>
+        <div class="flex flex-col gap-4">
+          <h3>Contact Sales</h3>
+          <a class="link" href="contact/index.html">Source code</a>
+          <a class="link" href="contact/live.html">Page source</a>
+        </div>
+      </div>
     </div>
   </section>
 </main>
